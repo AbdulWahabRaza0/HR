4c26832640514646f3b8d767871c39ef55f4bb9a
AbdulWahabRaza0 <wahab@authornate.com>
2023-11-07 15:12:29 +0500

    set up department module and implement all and create department end points

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
+import { DepartmentModule } from './department/department.module';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     EmployeeModule,
     AuthModule,
+    DepartmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
