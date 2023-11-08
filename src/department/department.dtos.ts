94ec2d8843f9391ca8bdeadc29025e9d2dd211a8
AbdulWahabRaza0 <wahab@authornate.com>
2023-11-08 11:48:55 +0500

    set up dtos for update department

--- src/department/department.dtos.ts
+++ src/department/department.dtos.ts
@@ -14,3 +14,34 @@ export class CreateDeptDto {
   @IsEmail()
   readonly email: string;
 }
+export class UpdateDeptDto {
+  @IsOptional()
+  @IsString()
+  readonly name: string;
+
+  @IsOptional()
+  @IsString()
+  readonly profileImg: string;
+
+  @IsOptional()
+  @IsString()
+  readonly contact: string;
+
+  @IsOptional()
+  @IsEmail()
+  readonly email: string;
+
+  @IsOptional()
+  @IsString()
+  readonly emergencyContact: string;
+}
+export class UpdateDeptRequestDto {
+  data: { UpdateDeptDto };
+}
+export class IdQuery {
+  @IsString()
+  readonly contact: string;
+}
+export class IdQueryRequestDto {
+  id: IdQuery;
+}
