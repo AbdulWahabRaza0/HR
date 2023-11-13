5d9105ea2e79841c9a34cbec75499868147dab3d
AbdulWahabRaza0 <wahab@authornate.com>
2023-11-13 14:43:54 +0500

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+  const config = new DocumentBuilder()
+    .setTitle('HRMS')
+    .setDescription('The is swagger documentation related to the NEST APIs')
+    .setVersion('1.0')
+    .addTag('HRMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port, () => {
     console.log('listening to port ', port);
   });
