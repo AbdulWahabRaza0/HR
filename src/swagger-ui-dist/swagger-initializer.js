bc64b11401f9e3e164221be0d19b5987ec748e78
AbdulWahabRaza0 <wahab@authornate.com>
2023-12-07 13:31:26 +0500

    made swagger live on production

--- src/swagger-ui-dist/swagger-initializer.js
+++ src/swagger-ui-dist/swagger-initializer.js
@@ -1,15 +1,11 @@
 window.onload = function () {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: '/api-json',
-    dom_id: '#swagger-ui',
+    url: "/api/docs.json", // Replace with the route to your Swagger JSON data
+    dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
     plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-    layout: 'StandaloneLayout',
+    layout: "StandaloneLayout",
   });
-
-  //</editor-fold>
 };
+
